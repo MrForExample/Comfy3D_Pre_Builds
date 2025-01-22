0c256fbebeb5c9fe4c3c2223690056481e56eb1e
Mr. For Example <62230687+MrForExample@users.noreply.github.com>
2025-01-22 15:35:46 -0600

    Update Custom Rasterizer source files for Hunyuan3D V2

--- _Libs/hunyuan3d_v2_custom_rasterizer/custom_rasterizer/render.py
+++ _Libs/hunyuan3d_v2_custom_rasterizer/custom_rasterizer/render.py
@@ -22,13 +22,13 @@
 # fine-tuning enabling code and other elements of the foregoing made publicly available
 # by Tencent in accordance with TENCENT HUNYUAN COMMUNITY LICENSE AGREEMENT.
 
-import hunyuan3d_v2_custom_rasterizer_kernel
+import custom_rasterizer_kernel
 import torch
 
 
 def rasterize(pos, tri, resolution, clamp_depth=torch.zeros(0), use_depth_prior=0):
     assert (pos.device == tri.device)
-    findices, barycentric = hunyuan3d_v2_custom_rasterizer_kernel.rasterize_image(pos[0], tri, clamp_depth, resolution[1],
+    findices, barycentric = custom_rasterizer_kernel.rasterize_image(pos[0], tri, clamp_depth, resolution[1],
                                                                      resolution[0], 1e-6, use_depth_prior)
     return findices, barycentric
 
--- _Libs/hunyuan3d_v2_custom_rasterizer/setup.py
+++ _Libs/hunyuan3d_v2_custom_rasterizer/setup.py
@@ -5,7 +5,7 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 # build with `python setup.py install`
 # nvcc is needed
 
-custom_rasterizer_module = CUDAExtension('hunyuan3d_v2_custom_rasterizer_kernel', [
+custom_rasterizer_module = CUDAExtension('custom_rasterizer_kernel', [
     'cpp_lib/custom_rasterizer_kernel/rasterizer.cpp',
     'cpp_lib/custom_rasterizer_kernel/grid_neighbor.cpp',
     'cpp_lib/custom_rasterizer_kernel/rasterizer_gpu.cu',
@@ -14,7 +14,7 @@ custom_rasterizer_module = CUDAExtension('hunyuan3d_v2_custom_rasterizer_kernel'
 setup(
     packages=find_packages(),
     version='0.1',
-    name='hunyuan3d_v2_custom_rasterizer',
+    name='custom_rasterizer',
     include_package_data=True,
     package_dir={'': '.'},
     ext_modules=[
